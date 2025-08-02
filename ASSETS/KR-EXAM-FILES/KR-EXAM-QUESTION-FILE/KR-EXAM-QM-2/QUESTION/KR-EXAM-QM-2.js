9587dac4f2f0f9932f35ea5a3fd269b29d5c976a
Rxrosan <rkc242855@gmail.com>
2025-08-02 19:38:58 +0545

    images added

--- ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-2/QUESTION/KR-EXAM-QM-2.js
+++ ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-2/QUESTION/KR-EXAM-QM-2.js
@@ -1,59 +1,59 @@
 window.questions = [
   {
      question: "[1-2] 그림을 보고 단어나 문장을 고르십시오.",
-     options: { a: "신용카드", b: "지폐", c: "동전", d: "현금" },
+     options: { a: "이불", b: "방", c: "휴식", d: "침대" },
      answer: "d",
      explanation: "Review not Aviable.",
-     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-1.png"
+     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-19.png"
    },
    {
      question: "",
-     options: { a: "학교", b: "공장", c: "회사", d: "은행" },
+     options: { a: "선생", b: "엄마", c: "언니 / 누나", d: "학교" },
      answer: "a",
      explanation: "Review not Aviable.",
-     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-2.png"
+     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-21.png"
    },
    {
-     question: "[3-4] 다음 설명에 알맞은 어휘를 고르십시오.<br>회사 , 공장 , 학고 등 일성한 장소에 근무나 일을 하기 위해 가는 것을 의미합니다.",
-     options: { a: "퇴근 ", b: "참석", c: "출근", d: "경험" },
+     question: "[3-4] 다음 단어와 비슷한 말을 무엇입니까?<br> <br> 올해",
+     options: { a: "다음해", b: "금주", c: "금년", d: "모처럼" },
      answer: "c",
      explanation: "Review not Aviable."
    },
    {
-     question: "아픈 사람들을 의사와 간호사가 치료하고 돌보는 장소입니다.",
-     options: { a: "극장", b: "병원", c: "은행", d: "식당" },
+     question: "다음 단어와 관계있는 것은 무엇입니까?<br> <br>미역국 , 케이크",
+     options: { a: "추석", b: "생신", c: "설날", d: "생산" },
      answer: "b",
      explanation: "Review not Aviable."
    },
    {
-     question: "[5-6]다음 질문에 답하십시오.<br>다음 파이 차트에 대한 설명으로 가장 알맞은 갓을 고르십시오.",
-     options: { a: "귤보다 감이 더 많이 발립니다.", b: "바나나 가장 작은 사람이 좋아핮니다.", c: "바나나는 두 번째로 많이 팔맂니다.", d: "포도를 좋아하는 사람이 가장 많습니다." },
-     answer: "d",
+     question: "[5-6]이 사람의 직업은 무엇은 입니까?",
+     options: { a: "목수 입니다.", b: "근로자 입니다.", c: "점원 입니다.", d: "기술자 입니다." },
+     answer: "a",
      explanation: "Review not Aviable.",
-     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-3.png"
+     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-24.png"
    },
    {
      question: "다음 밑줄 친 부분이 맞는 문장을 고르십시오.",
-     options: { a: "할머니깨서 음식을 <u>만들십니다</u>.", b: "<u>책이</u> 읽어요.", c: "다라 씨는 <u>회사에</U> 있어요.", d: "학교에 <u>가아요</u>." },
-     answer: "c",
+     options: { a: "불고기를 <u>만들아서</u> 먹었어요.", b: "고항에 가서 <u>부모님을</u> 만났어요.", c: "편지를 쓰서 친구에게 <u>줬어요</U>.", d: "아침에 <u>일어났어서</u> 운동했어요." },
+     answer: "b",
      explanation: "Review not Aviable."
    },
    {
      question: "[7-8] 다음 질문에 답하십시오.<br>다음 단어와 관계 있는 무엇입니까?<br>  <br>분뉴사",
      options: { a: "마리", b: "빵", c: "두", d: "명사" },
-     answer: "a",
+     answer: "c",
      explanation: "Review not Aviable."
    },
    {
      question: "다음 단어와 관계있는  것은 무엇입니까?<br>  <br>가방",
      options: { a: "나라", b: "물건", c: "장소", d: "음식" },
-     answer: "b",
+     answer: "d",
      explanation: "Review not Aviable."
    },
    {
      question: "[9-10] 다음 글을 내용과 같은 것을 고르십시오.<br>  <br>중고 거랫품을 이용하면 싸게 구할 수 있습니다. 생활용뿐만 아니라 가전제품, 가구에 이르기까지 다양한 물건을 살 수 있습니다. 사용하지 않은 새 물건을 파는 경우도 있습니다.플랫품을 통해 물건을 사고팔 때는 주로 같은 지역에 서는 사람들 사이에서 직거래로 이루어집니다.",
      options: { a: "물건 거래할 때 대부분 택배를 이용해서 물건을 받습니다. ", b: "중고 거래 플랫품에서 가구와 같은 물건은 구할 수 없습니다.", c: "중고 거래 플랫품으로 새 물건을 구입할 수 있습니다.", d: "중고 거래 플랫품에서 비싼 가격에 물건이 거래됩니다." },
-     answer: "c",
+     answer: "d",
      explanation: "Review not Aviable."
    },
    {
@@ -65,40 +65,40 @@ window.questions = [
  {
      question: "[11-14] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.<br>  <br> 머리가 너무 길어요. 내일 머리를 자르러 ---------- 에 갈 거에요.",
      options: { a: "우체국", b: "편의점", c: "커피솝", d: "미용실" },
-     answer: "d",
+     answer: "b",
      explanation: "Review not Aviable."
    },
    {
      question: "다음 단어와 관계있는 것은 무엇입니까?<br>  <br>토요일 , 일요일 ",
      options: { a: "주말", b: "요일 ", c: "평일", d: "월" },
-     answer: "a",
+     answer: "c",
      explanation: "Review not Aviable."
    },
    {
      question: "빈칸에 들어갈 가장 알맞은 것을 고르십시오.<br>  <br>김찌개가 너무 ---------- 조심해서 천천히 드세요.",
      options: { a: "비쌉니다", b: "뜨겁습니다", c: "맛있습니다", d: "적습니다" },
-     answer: "b",
+     answer: "c",
      explanation: "Review not Aviable."
    },
    {
      question: "빈칸에 들어갈 가장 알맞은 것을 고르십시오.<br>  <br>저는 집안일 중에서 정리하는 것을 ---------- 좋아합니다.",
      options: { a: "보통", b: "자주", c: "가장", d: "항상" },
-     answer: "c",
+     answer: "b",
      explanation: "Review not Aviable."
    },
    {
      question: "[15-18] 다음 질문에 답하십시오.<br>  <br> 이것은 무엇입니까?",
      options: { a: "우유 입니다.", b: "우체국 입니다.", c: "우산 입니다.", d: "유산 입니다." },
-     answer: "c",
+     answer: "d",
      explanation: "Review not Aviable.",
-     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-4.png"
+     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-20.png"
    },
    {
      question: "이 사람의 주소는 어디입니까?",
      options: { a: "서울특별시 구로구청장 입니다.", b: "서울특별시 구로구 입니다.", c: "서울특별시 구로구청장 입니다.", d: "2022년 2월 8일 입니다." },
-     answer: "b",
+     answer: "a",
      explanation: "Review not Aviable.",
-     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-5.png"
+     image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-22.png"
    },
    {
      question: "이것은 무엇입니까?",
@@ -123,7 +123,7 @@ window.questions = [
    {
      question: "두 사람은 무엇에 대해 말하고 있습니까?<br>  <br>남 : 가방에 뭐가 있어요 ?<br>여 : 열쇠하고 지갑이 있어요.그리고 여권도 있어요.",
      options: { a: "직업", b: "나이", c: "물건", d: "국적" },
-     answer: "c",
+     answer: "b",
      explanation: "Review not Aviable."
    },
    {
@@ -137,7 +137,7 @@ window.questions = [
    {
      question: "",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "c",
+     answer: "a",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-9.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-2.mp3"
@@ -145,7 +145,7 @@ window.questions = [
    {
      question: "",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "d",
+     answer: "c",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-10.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-3.mp3"
@@ -153,7 +153,7 @@ window.questions = [
    {
      question: "",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "c",
+     answer: "b",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-11.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-4.mp3"
@@ -169,7 +169,7 @@ window.questions = [
    {
      question: "여기는 어디 입니까?",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "b",
+     answer: "d",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-13.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-6.mp3"
@@ -177,7 +177,7 @@ window.questions = [
    {
      question: "이 사람은 무엇을 하고 있습니까?",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "d",
+     answer: "c",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-14.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-7.mp3"
@@ -185,7 +185,7 @@ window.questions = [
    {
      question: "맥주가 얼마나 있습니까?",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "c",
+     answer: "b",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-15.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-8.mp3"
@@ -193,7 +193,7 @@ window.questions = [
    {
      question: "이것은 무엇입니까?",
      options: { a: "", b: "", c: "", d: "" },
-     answer: "a",
+     answer: "d",
      explanation: "Review not Aviable.",
      image: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-ALL-IMG/IMG-16.png",
      audio: "ASSETS/KR-EXAM-FILES/KR-EXAM-QUESTION-FILE/KR-EXAM-QM-1/AUDIO/AUDIO-9.mp3"
