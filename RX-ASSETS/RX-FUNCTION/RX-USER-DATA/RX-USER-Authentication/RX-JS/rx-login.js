a6b2e6e61b299e3b627abdc7dfb89b52791f38b8
RX2061 <rkc242855@gmail.com>
2026-07-21 11:53:12 +0545

    update

--- RX-ASSETS/RX-FUNCTION/RX-USER-DATA/RX-USER-Authentication/RX-JS/rx-login.js
+++ RX-ASSETS/RX-FUNCTION/RX-USER-DATA/RX-USER-Authentication/RX-JS/rx-login.js
@@ -23,7 +23,7 @@
             }
 
             try {
-                const response = await fetch('http://localhost:3000/api/login', {
+                const response = await fetch('https://rosan-xettri.onrender.com/api/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password })
