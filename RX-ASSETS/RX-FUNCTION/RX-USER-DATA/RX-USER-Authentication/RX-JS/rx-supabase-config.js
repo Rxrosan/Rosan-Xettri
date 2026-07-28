c2b83e5f8ead6545b9270e15a6b22130d65137f5
RX2061 <rkc242855@gmail.com>
2026-07-28 12:57:49 +0545

    update

--- RX-ASSETS/RX-FUNCTION/RX-USER-DATA/RX-USER-Authentication/RX-JS/rx-supabase-config.js
+++ RX-ASSETS/RX-FUNCTION/RX-USER-DATA/RX-USER-Authentication/RX-JS/rx-supabase-config.js
@@ -1,7 +1,7 @@
 // rx-supabase-config.js
 
-const SUPABASE_URL = "https://xorxoovezlgqcaeyqpdp.supabase.co"; // आफ्नो Project URL यहाँ राख्नुहोस्
-const SUPABASE_ANON_KEY = "sb_publishable_5_yPXUnjJVe3dy13X5nkXQ_afJ7rCvM";
+const SUPABASE_URL = "https://svwwbxbyutiieflxnoeb.supabase.co"; // आफ्नो Project URL यहाँ राख्नुहोस्
+const SUPABASE_ANON_KEY = "sb_publishable_OBPxBVADXRdtjYEC_ZFcEw_95NR5UXA";
 
 // Supabase Client initialize गर्ने
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
